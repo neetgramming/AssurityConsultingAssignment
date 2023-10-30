4614a784c851d38e28132085aed9a85c693f86dc
neetgramming <madhushika.w@outlook.com>
2023-10-30 12:39:13 +0530

    test category completed attempt 2

--- src/main/java/com/neetgramming/request_util/CategoryRequestUtil.java
+++ src/main/java/com/neetgramming/request_util/CategoryRequestUtil.java
@@ -1,4 +1,4 @@
-package com.neetgramming;
+package com.neetgramming.request_util;
 
 import java.io.IOException;
 import java.net.URI;
@@ -6,19 +6,18 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class RequestUtil {
-    public static HttpResponse getResponse(String url) {
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-        HttpResponse httpResponse;
-        try {
-            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+public class CategoryRequestUtil {
+    public static HttpResponse<String> getResponse(String url) {
+        HttpResponse<String> httpResponse;
+        try (HttpClient httpClient = HttpClient.newHttpClient()) {
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+            try {
+                httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         return httpResponse;
     }
--- src/test/java/category_tests/CategoryTest.java
+++ src/test/java/category_tests/CategoryTest.java
@@ -1,28 +1,30 @@
-package api_verification_package;
+package category_tests;
 
-import com.neetgramming.RequestUtil;
+import com.jayway.jsonpath.JsonPath;
+import com.neetgramming.request_util.CategoryRequestUtil;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.net.http.HttpResponse;
 
-public class APIVerification {
+public class CategoryTest {
     private static final String REQ_URL = "https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false";
-    private SoftAssert softAssert = new SoftAssert();
+    private final SoftAssert softAssert = new SoftAssert();
     @Test
-    public void testVerifyCatelog() {
-        //send the request and get the response
-        HttpResponse response = RequestUtil.getResponse(REQ_URL);
+    public void testCategory() {
+        HttpResponse<String> response = CategoryRequestUtil.getResponse(REQ_URL);
+        String responseBody = response.body();
 
-        JSONObject jsonObject = new JSONObject(response.body().toString());
-        jsonObject.get("Promotions.Name[]");
+        String actualName = JsonPath.parse(responseBody).read("$.Name");
+        boolean actualCanReList = (Boolean)(JsonPath.parse(responseBody).read("$.CanRelist"));
+        boolean actualDescriptionContains = JsonPath.parse(responseBody).
+                read("$.Promotions[?(@.Name=='Gallery')].Description").toString().
+                contains("Good position in category");
 
-        System.out.println("conn");
-        //verify name ==
-        softAssert.assertEquals(jsonObject.get("Name"), "Carbon credits", "Incorrect Name");
-        softAssert.assertTrue(jsonObject.get("CanReList"), "Carbon credits", "Incorrect Name");
-        //verify canRelist ==
-        //verify nameDesc contains
+        softAssert.assertEquals(actualName, "Carbon credits", "Incorrect Name");
+        softAssert.assertTrue(actualCanReList, "CanRelist is false");
+        softAssert.assertTrue(actualDescriptionContains,  "Description in Gallery NOT contains the text 'Good position in category'");
+        softAssert.assertAll();
     }
 }
